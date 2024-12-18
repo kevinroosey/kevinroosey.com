730c23a6ed2f6ba18cd3e2f2adeaf649bd3aedbc
Kevin <kevinroosey@gmail.com>
2024-12-17 20:13:05 -0600

    update

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -6,10 +6,10 @@ export default function Links() {
     return (
         <div className="bg-gradient-to-t from-neutral-800 to-neutral-700 border-2 border-neutral-500 h-fit w-fit rounded-xl flex justify-center items-center bottom-5 fixed gap-1 lg:gap-4 px-1 lg:px-4 py-2 mx-auto right-0 left-0">
             <div>
-                <a href="https://x.com/itskevin" className="text-white">
-                <div className="flex gap-1 items-center">
-                    <Icons.twitter className="h-4 w-4 text-white" />
-                    <span className="text-white text-xs">@itskevin</span>
+                <a href="https://x.com/itskevin" className="text-neutral-200 hover:text-white">
+                <div className="flex gap-1 items-center text-neutral-200 hover:text-white">
+                    <Icons.twitter className="h-4 w-4" />
+                    <span className=" text-xs">@itskevin</span>
                 </div>
                 </a>
             </div>
@@ -17,9 +17,9 @@ export default function Links() {
             <MenuSeparator className="h-5 w-0.5 bg-neutral-600" />
             <div className="">
                 <a href="mailto:kevinroosey@gmail.com" className="text-white">
-                    <div className="flex gap-1 items-center">
+                    <div className="flex gap-1 items-center text-neutral-200 hover:text-white">
                         <EnvelopeIcon className="h-5 w-5" />
-                        <span className="text-white text-xs">kevinroosey@gmail.com</span>
+                        <span className="text-xs">kevinroosey@gmail.com</span>
                     </div>
                 </a>
             </div>
@@ -27,9 +27,9 @@ export default function Links() {
             <MenuSeparator className="h-5 w-0.5 bg-neutral-600" />
             <div>
                 <a href="https://github.com/kevinroosey" className="text-white">
-                    <div className="flex gap-1 items-center">
+                    <div className="flex gap-1 items-center text-neutral-200 hover:text-white">
                         <Icons.gitHub />
-                        <span className="text-white text-xs">kevinroosey</span>
+                        <span className="text-xs">kevinroosey</span>
                     </div>
                 </a>
             </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,9 @@
 export default function Header() {
     return (
-        <div className="w-full m-auto">
+        <div className="w-full m-auto flex gap-4">
+            <div>
+                <img src="./PersonalImage.png" alt="Kevin Roosey" className="w-16 h-16 rounded-xl" width={100} height={100} />
+            </div>
             <div className="flex flex-col gap-4">
                 <div>
                     <h1 className="text-left text-xl font-semibold text-primary-950">Kevin Roosey</h1>
