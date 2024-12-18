a105c837bb78cea3b8e401e52e8a850cf9c7c764
Kevin <kevinroosey@gmail.com>
2024-12-17 19:14:02 -0600

    update

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -6,7 +6,7 @@ export default function Links() {
     return (
         <div className="bg-gradient-to-t from-neutral-800 to-neutral-700 border-2 border-neutral-500 h-fit w-fit rounded-xl flex justify-center items-center bottom-5 fixed gap-1 lg:gap-4 px-1 lg:px-4 py-2 mx-auto right-0 left-0">
             <div>
-                <a href="/x.com/itskevin" className="text-white">
+                <a href="https://x.com/itskevin" className="text-white">
                 <div className="flex gap-1 items-center">
                     <Icons.twitter className="h-4 w-4 text-white" />
                     <span className="text-white text-xs">@itskevin</span>
@@ -16,7 +16,7 @@ export default function Links() {
 
             <MenuSeparator className="h-5 w-0.5 bg-neutral-600" />
             <div className="">
-                <a href="mailto@kevinroosey@gmail.com" className="text-white">
+                <a href="mailto:kevinroosey@gmail.com" className="text-white">
                     <div className="flex gap-1 items-center">
                         <EnvelopeIcon className="h-5 w-5" />
                         <span className="text-white text-xs">kevinroosey@gmail.com</span>
@@ -26,7 +26,7 @@ export default function Links() {
             
             <MenuSeparator className="h-5 w-0.5 bg-neutral-600" />
             <div>
-                <a href="mailto@kevinroosey@gmail.com" className="text-white">
+                <a href="https://github.com/kevinroosey" className="text-white">
                     <div className="flex gap-1 items-center">
                         <Icons.gitHub />
                         <span className="text-white text-xs">kevinroosey</span>
